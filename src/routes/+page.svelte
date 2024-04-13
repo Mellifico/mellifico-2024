b3d6a0cdd4a5e9ec6d79b5c29145d4888bfd9771
Melli <mellifico@gmail.com>
2024-04-13 16:32:27 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <main class="container mx-auto p-4">
 <section>
 <h1>Philippe Michou,  développeur et formateur Web</h1>
-<p>Ce site est en refonte complète, merci de votre compréhension.</p>
+<p>Ce site est en refonte complète, merci de votre compréhension. Plus d'infos sur mon compte github : https://github.com/Mellifico</p>
 <p>Pour toute question professionnelle, veuillez me contacter via la plateforme Malt. Vous trouverez mon profil <a class="underline decoration-2" href="https://www.malt.fr/profile/mellifico" title="https://www.malt.fr/profile/mellifico">ici</a>.</p>
 <p>Merci de votre visite.</p>
 </section>
